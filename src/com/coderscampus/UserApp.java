4e778274ec5c637e43e1a200d7da2050db14a65b
tasmith76 <timothy.smith76@gmail.com>
2021-12-01 14:52:22 -0800

    commit

--- src/com/coderscampus/UserApp.java
+++ src/com/coderscampus/UserApp.java
@@ -11,7 +11,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
-public class UserApp {
+public class UserApp { 
 
 	public static void main(String[] args) throws FileNotFoundException {
 		List<User> loginID = new ArrayList<>();
